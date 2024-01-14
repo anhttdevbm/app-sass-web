00eb606c004021cff2f9d6666cac929fd497c56e
Toan Tran Cong <22110248@student.hcmute.edu.vn>
2024-01-14 11:47:12 +0700

    Update: Change page in docs section

--- src/components/sn-docs/detail/LeftSlide/ItemDocs.tsx
+++ src/components/sn-docs/detail/LeftSlide/ItemDocs.tsx
@@ -11,95 +11,119 @@ import MorePoper from "./MorePoper";
 import { IDocument } from "constant/types";
 
 export interface ItemDocsProps {
-    onClick?: any;
-    title: string| undefined;
-    project_id?: string;
-    id?: string;
-    children: ItemDocsProps[] | undefined;
+  onClick?: any;
+  handleChangeDocument?: any;
+  title: string | undefined;
+  project_id?: string;
+  id?: string;
+  children: ItemDocsProps[] | undefined;
 }
 type TDocumentListProps = {
-    isFirst?: boolean;
-    data: IDocument;
-    onClick: any;
+  isFirst?: boolean;
+  data: IDocument;
+  onClick: any;
+  handleChangeDocument: any;
 };
 
-
-const Document = ({ title, id, project_id, onClick }: ItemDocsProps) => {
-    return (
-        <>
-            <Box
-                sx={{
-                    cursor: "pointer",
-                    display: "flex",
-                    alignItems: "center",
-                    justifyContent: "space-between",
-                    padding: 1,
-                    ":hover": {
-                        " .btn-more": {
-                            visibility: "visible",
-                        },
-                    },
-                }}
-            >
-                <Box
-                    sx={{
-                        display: "flex",
-                        alignItems: "center",
-                        gap: 1,
-                    }}
-                >
-                    <DocsItem></DocsItem>
-                    <Text sx={{ flex: 1 }} fontWeight={600} fontSize={14}>
-                        {title}
-                    </Text>
-                </Box>
-                <Box
-                    className="btn-more"
-                    sx={{
-                        visibility: "hidden",
-                        display: "flex",
-                        alignItems: "center",
-                        gap: 1.4,
-                    }}
-                >
-                    <MorePoper></MorePoper>
-                    <Box
-                        onClick={() => onClick(id, project_id)}
-                        sx={{
-                            cursor: "pointer",
-                        }}
-                    >
-                        <IconAdd></IconAdd>
-                    </Box>
-                </Box>
-            </Box>
-        </>
-    );
+const Document = ({
+  title,
+  id,
+  project_id,
+  onClick,
+  handleChangeDocument,
+}: ItemDocsProps) => {
+  return (
+    <>
+      <Box
+        sx={{
+          cursor: "pointer",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+          padding: 1,
+          ":hover": {
+            " .btn-more": {
+              visibility: "visible",
+            },
+          },
+        }}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            gap: 1,
+          }}
+          onClick={() => handleChangeDocument(id)}
+        >
+          <DocsItem></DocsItem>
+          <Text sx={{ flex: 1 }} fontWeight={600} fontSize={14}>
+            {title}
+          </Text>
+        </Box>
+        <Box
+          className="btn-more"
+          sx={{
+            visibility: "hidden",
+            display: "flex",
+            alignItems: "center",
+            gap: 1.4,
+          }}
+        >
+          <MorePoper></MorePoper>
+          <Box
+            onClick={() => onClick(id, project_id)}
+            sx={{
+              cursor: "pointer",
+            }}
+          >
+            <IconAdd></IconAdd>
+          </Box>
+        </Box>
+      </Box>
+    </>
+  );
 };
 
-const DocumentList: React.FC<TDocumentListProps> = ({ data, onClick }) => {
-    return (
-        <>
-            <Document onClick={onClick} children={data?.child} id={data?.id} project_id={data?.project_id} title={data?.name} />
-            {data?.child && (
-                <Box
-                    sx={{
-                        marginLeft: {
-                            md: "24px",
-                            xs: "16px",
-                        },
-                    }}
-                >
-                    {Array.isArray(data?.child) &&
-                        data?.child?.map((item: IDocument) => {
-                            return (
-                                <DocumentList onClick={onClick} key={item._id} data={item} />
-                            );
-                        })}
-                </Box>
-            )}
-        </>
-    );
+const DocumentList: React.FC<TDocumentListProps> = ({
+  data,
+  onClick,
+  handleChangeDocument,
+}) => {
+  return (
+    <>
+      <Document
+        onClick={onClick}
+        children={data?.child}
+        id={data?.id}
+        project_id={data?.project_id}
+        handleChangeDocument={handleChangeDocument}
+        title={data?.name}
+      />
+      {data?.child && (
+        <Box
+          sx={{
+            marginLeft: {
+              md: "24px",
+              xs: "16px",
+            },
+          }}
+        >
+          {Array.isArray(data?.child) &&
+            data?.child?.map((item: IDocument) => {
+              return (
+                <DocumentList
+                  onClick={onClick}
+                  key={item._id}
+                  data={item}
+                  handleChangeDocument={handleChangeDocument}
+                />
+              );
+            })}
+        </Box>
+      )}
+    </>
+  );
 };
 
 export default memo(DocumentList);
--- src/components/sn-docs/detail/LeftSlide/LeftSlideDoc.tsx
+++ src/components/sn-docs/detail/LeftSlide/LeftSlideDoc.tsx
@@ -13,151 +13,162 @@ import { uuid } from "utils/index";
 import useTheme from "hooks/useTheme";
 import { useGetDocDetailQuery, useCreateDocMutation } from "store/docs/api";
 import { useParams } from "next/navigation";
+import { useDocs } from "store/docs/selectors";
 
 export interface LeftSlideDocProps {
-    open: boolean;
-    setOpen?: any;
+  open: boolean;
+  setOpen?: any;
 }
 
 const LeftSlideDoc = ({ open, setOpen }: LeftSlideDocProps) => {
-    const dataFake = useAppSelector((state) => state.doc.docDetails.data);
-    const { id } = useParams();
-    const { data: document, isLoading } = useGetDocDetailQuery(id as string);
-    const [createDoc, { data: docsData, error }] = useCreateDocMutation();
+  const dataFake = useAppSelector((state) => state.doc.docDetails.data);
+  const { id } = useParams();
+  const { data: document, isLoading } = useGetDocDetailQuery(id as string);
+  const [createDoc, { data: docsData, error }] = useCreateDocMutation();
 
-    const [data, setData] = useState({});
-    const docsT = useTranslations(NS_DOCS);
-    const [search, setSearch] = useState("");
-    const onChangeQueries = (name: string, value: any) => {
-        setSearch(value);
+  const [data, setData] = useState({});
+  const docsT = useTranslations(NS_DOCS);
+  const [search, setSearch] = useState("");
+  const onChangeQueries = (name: string, value: any) => {
+    setSearch(value);
+  };
+
+  useEffect(() => {
+    setData(document);
+  }, [document]);
+
+  const addChildToData = (parent, child) => {
+    console.log("parent", parent);
+    const newData = { ...data };
+    const addChildToParent = async (parentNode) => {
+      const modifiedParentNode = {
+        ...parentNode,
+        child: parentNode.child || [],
+      };
+
+      if (modifiedParentNode.id === parent) {
+        modifiedParentNode.child = Array.isArray(modifiedParentNode.child)
+          ? [...modifiedParentNode.child, child]
+          : [child];
+        const result = await createDoc(child);
+      } else {
+        modifiedParentNode.child.forEach((child) => {
+          addChildToParent(child);
+        });
+      }
+    };
+    addChildToParent(newData);
+    setData(newData);
+  };
+
+  const { handleGetDocDetail } = useDocs();
+
+  const handleChangeDocument = (id) => {
+    handleGetDocDetail(id);
+  };
+
+  // Hàm xử lý khi nhấn nút "Thêm mục con"
+  const handleAddChild = (parent, project_id) => {
+    alert(parent);
+    const id = uuid();
+    const newChild = {
+      id: id,
+      project_id: project_id,
+      root_directory: parent,
+      name: "No name",
+      description: "",
     };
 
-    useEffect( () => {
-        setData(document)
-    }, [document])
+    addChildToData(parent, newChild);
+  };
 
-
-    const addChildToData = (parent, child) => {
-        console.log('parent', parent)
-        const newData = { ...data };
-
-        const addChildToParent = async (parentNode) => {
-
-            const modifiedParentNode = {
-                ...parentNode,
-                child: parentNode.child || []
-            };
-
-            if (modifiedParentNode.id === parent) {
-                modifiedParentNode.child = Array.isArray(modifiedParentNode.child) ? [...modifiedParentNode.child, child] : [child];
-                const result = await createDoc(child);
-            } else {
-                modifiedParentNode.child.forEach((child) => {
-                    addChildToParent(child);
-                });
-            }
-        };
-        addChildToParent(newData);
-        setData(newData);
-    };
-
-    // Hàm xử lý khi nhấn nút "Thêm mục con"
-    const handleAddChild = (parent, project_id) => {
-        const id = uuid();
-        const newChild = {
-            id: id,
-            project_id: project_id,
-            root_directory : parent,
-            name: "No name",
-            description: "",
-        };
-
-        addChildToData(parent, newChild);
-    };
-
-    return (
+  return (
+    <Box
+      sx={{
+        position: "relative",
+        width: {
+          sm: "30%",
+          xs: open ? "236px" : "0",
+        },
+      }}
+    >
+      <Box
+        sx={{
+          position: "relative",
+          height: "100%",
+          backgroundColor: {
+            sm: "unset",
+            xs: "background.paper",
+          },
+          width: "100%",
+          zIndex: "39",
+          display: "block",
+        }}
+      >
         <Box
-            sx={{
-                position: "relative",
-                width: {
-                    sm: "30%",
-                    xs: open ? "236px" : "0",
-                },
-            }}
+          onClick={() => setOpen((value) => !value)}
+          sx={{
+            position: "absolute",
+            top: 0,
+            left: open ? "calc(100% + 4px)" : "calc(100% - 16px)",
+            padding: "4px",
+            display: {
+              sm: "none",
+              xs: "flex",
+            },
+            zIndex: "30",
+            justifyContent: "center",
+            alignItems: "center",
+            backgroundColor: "#E1F0FF",
+            transform: open ? "rotate(180deg)" : "unset",
+          }}
         >
-            <Box
-                sx={{
-                    position: "relative",
-                    height: "100%",
-                    backgroundColor: {
-                        sm: "unset",
-                        xs: "background.paper",
-                    },
-                    width: "100%",
-                    zIndex: "39",
-                    display: "block",
-                }}
-            >
-                <Box
-                    onClick={() => setOpen((value) => !value)}
-                    sx={{
-                        position: "absolute",
-                        top: 0,
-                        left: open ? "calc(100% + 4px)" : "calc(100% - 16px)",
-                        padding: "4px",
-                        display: {
-                            sm: "none",
-                            xs: "flex",
-                        },
-                        zIndex: "30",
-                        justifyContent: "center",
-                        alignItems: "center",
-                        backgroundColor: "#E1F0FF",
-                        transform: open ? "rotate(180deg)" : "unset",
-                    }}
-                >
-                    <ArrowRight />
-                </Box>
-                <Box
-                    sx={{
-                        display: {
-                            sm: "block",
-                            xs: open ? "block" : "none",
-                        },
-                        width: {
-                            sm: "auto",
-                            xs: open ? "200px" : "0",
-                        },
-                    }}
-                >
-                    <Search
-                        placeholder={docsT("filter.search", { name: "email" })}
-                        name="doc"
-                        onChange={onChangeQueries}
-                        value={search}
-                        sx={{
-                            width: "100%",
-                            ".MuiOutlinedInput-root": {
-                                backgroundColor: "background.paper",
-                            },
-                        }}
-                    />
-                    <Box>
-                        <Text
-                            sx={{
-                                marginTop: "8px",
-                            }}
-                            color={"grey"}
-                            variant={"h6"}
-                        >
-                            Page
-                        </Text>
-                        {/* <DocumentList onClick={handleAddChild} data={data} /> */}
-                    </Box>
-                </Box>
-            </Box>
+          <ArrowRight />
         </Box>
-    );
+        <Box
+          sx={{
+            display: {
+              sm: "block",
+              xs: open ? "block" : "none",
+            },
+            width: {
+              sm: "auto",
+              xs: open ? "200px" : "0",
+            },
+          }}
+        >
+          <Search
+            placeholder={docsT("filter.search", { name: "email" })}
+            name="doc"
+            onChange={onChangeQueries}
+            value={search}
+            sx={{
+              width: "100%",
+              ".MuiOutlinedInput-root": {
+                backgroundColor: "background.paper",
+              },
+            }}
+          />
+          <Box>
+            <Text
+              sx={{
+                marginTop: "8px",
+              }}
+              color={"grey"}
+              variant={"h6"}
+            >
+              Page
+            </Text>
+            <DocumentList
+              onClick={handleAddChild}
+              handleChangeDocument={handleChangeDocument}
+              data={data}
+            />
+          </Box>
+        </Box>
+      </Box>
+    </Box>
+  );
 };
 
 export default memo(LeftSlideDoc);
