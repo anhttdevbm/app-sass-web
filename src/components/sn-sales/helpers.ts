bc01b67b0785bfd225c3256c6a6e48963a6491cc
nghianguyent <nghia14302@gmail.com>
2024-01-25 23:11:32 +0700

    Change exel export type

--- src/components/sn-sales/helpers.ts
+++ src/components/sn-sales/helpers.ts
@@ -89,7 +89,7 @@ export const CURRENCY_SYMBOL: Record<CURRENCY_CODE, string> = {
 export const EXPORT_TYPE_OPTIONS = {
   PDF: "pdf",
   CSV: "csv",
-  XLS: "xls",
+  XLS: "xlsx",
 };
 
 export const EXPORT_ORIENTATION_OPTIONS = {
