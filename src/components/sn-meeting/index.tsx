0711ab3740e213d58a7b88eafdfacb85a2284831
fiwallsld <thanhpromu@gmail.com>
2024-03-27 22:05:04 +0700

    Add MeetingRoom component

---
+++ src/components/sn-meeting/index.tsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import MeetingLayout from './meeting-layout/MeetingLayout';
+
+const MeetingRoom: React.FC = () => {
+  return (
+    <div>
+      <h1>Meeting Room</h1>
+      <MeetingLayout />
+    </div>
+  );
+};
+
+export default MeetingRoom;
