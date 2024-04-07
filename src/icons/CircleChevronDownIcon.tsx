34d890d166e62be05f489fb614952b1879558b98
Quang Dang <dangminhquang.r@gmail.com>
2024-04-07 23:16:54 +0700

    Fix svg attribute error

--- src/icons/CircleChevronDownIcon.tsx
+++ src/icons/CircleChevronDownIcon.tsx
@@ -15,8 +15,8 @@ const CircleChevronDownIcon = (props: SvgIconProps) => {
         d="M10 10.793 7.732 8.146a.387.387 0 0 0-.606 0 .56.56 0 0 0 0 .708l2.571 3a.387.387 0 0 0 .606 0l2.571-3a.56.56 0 0 0 0-.708.387.387 0 0 0-.606 0L10 10.793Z"
         fill="currentColor"/>
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        fillRule="evenodd"
+        clipRule="evenodd"
         d="M19.2 10A9.2 9.2 0 1 1 .8 10a9.2 9.2 0 0 1 18.4 0Zm-.2 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
         fill="currentColor"
       />
