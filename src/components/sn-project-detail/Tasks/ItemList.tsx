c6dd2ae047c74c500330dce62f9175f45c5ff5ed
miobi3698 <44633176+miobi3698@users.noreply.github.com>
2024-07-16 19:23:41 +0700

    Fixed: sort the tasklists and the tasks

--- src/components/sn-project-detail/Tasks/ItemList.tsx
+++ src/components/sn-project-detail/Tasks/ItemList.tsx
@@ -992,8 +992,12 @@ const ItemList = () => {
   }, [isFetching]);
 
   useEffect(() => {
-    items.sort((a, b) => a.id.localeCompare(b.id));
-    setDataList(items);
+    const _items = structuredClone(items);
+    _items.sort((a, b) => a.id.localeCompare(b.id));
+    for (const _item of _items) {
+      _item.tasks.sort((a, b) => a.id.localeCompare(b.id));
+    }
+    setDataList(_items);
   }, [items]);
 
   useEffect(() => {
