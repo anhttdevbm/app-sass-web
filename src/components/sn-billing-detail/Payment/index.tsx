1f6dddd75967e6687e25f6ba2262f2fcda6300cf
Tung Le <tung.lexuan@powergateasoftware.com>
2024-08-06 16:05:42 +0700

    fix bug tsx

--- src/components/sn-billing-detail/Payment/index.tsx
+++ src/components/sn-billing-detail/Payment/index.tsx
@@ -1,19 +1,19 @@
 import { Box, Stack, Typography } from "@mui/material";
+import { CURRENCY_SYMBOL } from "components/sn-sales/helpers";
+import { CURRENCY_CODE } from "constant/enums";
 import { NS_BILLING, NS_COMMON } from "constant/index";
 import useBreakpoint from "hooks/useBreakpoint";
 import { useTranslations } from "next-intl";
 import { useParams } from "next/navigation";
-import { memo, useEffect, useMemo, useState } from "react";
+import { memo, useEffect, useState } from "react";
 import { PaymentData } from "store/billing/actions";
 import { useBillings } from "store/billing/selectors";
+import { useInvoices } from "store/invoice/selectors";
+import { formatDate, formatNumber } from "utils/index";
 import "../Payment/PaymentTableHome/style.css";
+import DoughnutChartPayment from "../components/DoughnutChartPayment";
 import PaymentModal from "../components/PaymentModal";
 import PaymentTable from "./PaymentTable";
-import { formatDate, formatNumber } from "utils/index";
-import { CURRENCY_SYMBOL } from "components/sn-sales/helpers";
-import { CURRENCY_CODE } from "constant/enums";
-import DoughnutChartPayment from "../components/DoughnutChartPayment";
-import { useInvoices } from "store/invoice/selectors";
 
 type TabProps = {
   title: string;
@@ -159,6 +159,26 @@ const TabPayment = (props: TabProps) => {
   //       ? Math.round((sumAmountWriteOff / item?.amount) * 100)
   //       : 0;
   //   return data;
+  // }, [item, dataPayment]);
+
+  // const totalOfWriteOff = useMemo(() => {
+  //   const res = dataPayment?.reduce(
+  //     (sum, e: PaymentData) =>
+  //       e?.amount && e?.status == "Writeoff" ? sum + e?.amount : 0,
+  //     0,
+  //   );
+
+  //   return res;
+  // }, [item, dataPayment]);
+
+  // const totalOfPaid = useMemo(() => {
+  //   const res = dataPayment?.reduce(
+  //     (sum, e: PaymentData) =>
+  //       e?.amount && e?.status == "Paid" ? sum + e?.amount : 0,
+  //     0,
+  //   );
+
+  //   return res;
   // }, [item, dataPayment]);
 
   // const totalOfWriteOff = useMemo(() => {
