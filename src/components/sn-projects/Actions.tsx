bcb6047a78b66c7205fbfab0f962783c6b5c9fb5
miobi3698 <44633176+miobi3698@users.noreply.github.com>
2024-08-17 20:54:24 +0700

    Changed Status UI in Project Task List Page

--- src/components/sn-projects/Actions.tsx
+++ src/components/sn-projects/Actions.tsx
@@ -43,6 +43,7 @@ import {
 import Form, { ProjectDataForm } from "./Form";
 import useBreakpoint from "hooks/useBreakpoint";
 import { ExpandMore } from "@mui/icons-material";
+import StatusDropdown from "./components/StatusDropdown";
 
 const Actions = () => {
   const { filters, onGetProjects, pageSize, onCreateProject } = useProjects();
@@ -320,54 +321,6 @@ const Actions = () => {
 export default memo(Actions);
 
 const LATEST_VALUE = "updated_time=-1";
-
-const StatusDropdown = (props: {
-  value: ProjectStatus | "";
-  onChange: (value: ProjectStatus | "") => void;
-  sx?: SxProps;
-}) => {
-  const commonT = useTranslations(NS_COMMON);
-
-  return (
-    <TextField
-      select
-      size="small"
-      SelectProps={{
-        displayEmpty: true,
-        startAdornment: (
-          <InputAdornment position="start">
-            <Typography sx={{ color: "grey.600" }}>
-              {commonT("status")}:
-            </Typography>
-          </InputAdornment>
-        ),
-        IconComponent: (_props) => <ExpandMore {..._props} />,
-      }}
-      value={props.value}
-      onChange={(e) => props.onChange(e.target.value as ProjectStatus | "")}
-      sx={{
-        "& .MuiOutlinedInput-root": {
-          "& .MuiOutlinedInput-notchedOutline": {
-            borderRadius: "2rem",
-          },
-        },
-        ...props.sx,
-      }}
-    >
-      <MenuItem value="">{commonT("all")}</MenuItem>
-      {STATUS_OPTIONS.map((option) => (
-        <MenuItem key={option.value} value={option.value}>
-          <TextStatus
-            text={commonT(option.label)}
-            color={COLOR_STATUS[option.value]}
-          >
-            {commonT(option.label)}
-          </TextStatus>
-        </MenuItem>
-      ))}
-    </TextField>
-  );
-};
 
 const AssignerDropdown = (props: {
   value: Option | "";
