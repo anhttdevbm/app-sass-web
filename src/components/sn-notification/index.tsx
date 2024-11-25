8114ff0ce03ad70c1f903acaca2bcfe6bfe0cc55
longleu0902 <longleu0902@gmail.com>
2024-11-25 22:45:12 +0700

    settingg notification

--- src/components/sn-notification/index.tsx
+++ src/components/sn-notification/index.tsx
@@ -11,14 +11,19 @@ import { ItemNotification, PropsItemNotification } from "./type/type"
 
 const ItemNotification = (props: PropsItemNotification) => {
     const { data } = useGetHistoryNotification()
-    console.log("🚀 ~ ItemNotification ~ data:", data)
+    const { handleClose, activeTag } = props
 
-    const { handleClose } = props
+    const filterData = (activeTag: number) => {
+        if (activeTag == 2) {
+            const _data = data?.data?.DT?.filter((item: ItemNotification) => item?.readSatus == false)
+            return _data
+        }
+    }
 
-    console.log("check data >>>", data)
+
     return (
         <Stack sx={{ width: "100%" }} gap={2} direction="column">
-            {data?.data?.DT?.map((item: ItemNotification, index: number) => (
+            {filterData(activeTag)?.map((item: ItemNotification, index: number) => (
                 <Stack key={index}>
                     <DetaiList item={item} handleClose={handleClose} />
                 </Stack>
