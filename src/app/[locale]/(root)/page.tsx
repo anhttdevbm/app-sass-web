1ab1701b5c049bfaeba44e91025bc5a899989f4e
SangLM <Sanglm2207@gmail.com>
2024-12-06 21:07:30 +0700

    fix: enhance user role checks to conditionally render JoinWorkspace component alongside WaitingApprove and Dashboard

--- src/app/[locale]/(root)/page.tsx
+++ src/app/[locale]/(root)/page.tsx
@@ -2,21 +2,21 @@
 
 import Wrapper from "components/Wrapper";
 import Dashboard from "components/sn-dashboard/Dashboard";
-import WaitingApprove from "components/sn-waiting-approve/index"; // Import the WaitingApprove component
-import { useAuth } from "store/app/selectors"; // Assuming useAuth provides user information
-
-// export const metadata = {
-//   title: "Dashboard | Taskcover",
-// };
+import JoinWorkspace from "components/sn-join-workspace";
+import WaitingApprove from "components/sn-waiting-approve/index";
+import { Permission } from "constant/enums";
+import { useAuth } from "store/app/selectors";
 
 export default function Page() {
   const { user } = useAuth();
-  // Check the user's status and company
+
+  // Check the user's status, company, and roles
   const shouldShowWaitingApprove = user?.status === 2 && user?.company !== null;
+  const shouldShowJoinWorkspace = user?.status === 1 && user?.company !== null && user?.roles?.includes(Permission.EU);
 
   return (
     <Wrapper overflow="auto" spacing={3} transparent>
-      {shouldShowWaitingApprove ? <WaitingApprove /> : <Dashboard />}
+      { shouldShowJoinWorkspace ? <JoinWorkspace /> : shouldShowWaitingApprove ? <WaitingApprove /> : <Dashboard />}
     </Wrapper>
   );
 }
