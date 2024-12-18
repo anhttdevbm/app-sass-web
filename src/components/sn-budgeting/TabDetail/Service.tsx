8cfcf267e1241e0b67b6bfcfec82ecf30e1d8097
SangLM <Sanglm2207@gmail.com>
2024-12-19 02:52:27 +0700

    fix: reorder imports in Service component for better readability

--- src/components/sn-budgeting/TabDetail/Service.tsx
+++ src/components/sn-budgeting/TabDetail/Service.tsx
@@ -1,10 +1,10 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-empty-function */
 import { Box } from "@mui/material";
-import { ServiceAreaTotal } from "./Services/ServiceAreaTotal";
-import { ServiceAreaSection } from "./Services/ServiceAreaSection";
-import { ServiceSection } from "./Services/ServiceSection";
 import { TBudgetSection } from "../BudgetDetail";
+import { ServiceAreaSection } from "./Services/ServiceAreaSection";
+import { ServiceAreaTotal } from "./Services/ServiceAreaTotal";
+import ServiceSection from "./Services/ServiceSection";
 
 type Props = {
   isEdit?: boolean;
