14aeebf8043adfcc63992c1643a4d6eca6263e58
SangLM <Sanglm2207@gmail.com>
2024-12-28 14:41:50 +0700

    fix: enhance project name display with tooltip and ellipsis for better readability in DesktopCells

--- src/components/sn-projects/DesktopCells.tsx
+++ src/components/sn-projects/DesktopCells.tsx
@@ -1,18 +1,15 @@
-import { memo } from "react";
+import { Stack } from "@mui/material";
+import Avatar from "components/Avatar";
+import { Text, Tooltip } from "components/shared";
 import { BodyCell } from "components/Table";
+import { NS_COMMON } from "constant/index";
+import { PROJECT_TASKS_PATH } from "constant/paths";
+import { useTranslations } from "next-intl";
+import ProjectPlaceholderImage from "public/images/img-logo-placeholder.webp";
+import { memo } from "react";
 import { Project } from "store/project/reducer";
 import { getPath } from "utils/index";
-import { PROJECT_TASKS_PATH } from "constant/paths";
-import { formatDate } from "utils/index";
-import Avatar from "components/Avatar";
-import { Stack } from "@mui/material";
-import { Text } from "components/shared";
-import ProjectPlaceholderImage from "public/images/img-logo-placeholder.webp";
-import { Saved, SelectStatus, Assigner } from "./components";
-import { useTranslations } from "next-intl";
-import { NS_COMMON, DATE_LOCALE_FORMAT } from "constant/index";
-import dayjs from "dayjs";
-import DatePicker from "./components/DatePicker";
+import { Assigner, Saved, SelectStatus } from "./components";
 
 type DesktopCellsProps = {
   item: Project;
@@ -32,15 +29,24 @@ const DesktopCells = (props: DesktopCellsProps) => {
       >
         <Stack direction="row" alignItems="center" spacing={1}>
           <Avatar size={32} src={item.avatar ?? ProjectPlaceholderImage} />
-          <Text
-            variant="body2"
-            color="text.primary"
-            fontWeight={600}
-            lineHeight={1.28}
-            sx={{ "&:hover": { color: "primary.main" } }}
-          >
-            {item.name}
-          </Text>
+          <Tooltip title={item.name} placement="bottom-start">
+            <Text
+              variant="body2"
+              color="text.primary"
+              fontWeight={600}
+              lineHeight={1.28}
+              sx={{
+                "&:hover": { color: "primary.main" },
+                whiteSpace: "nowrap",
+                overflow: "hidden",
+                textOverflow: "ellipsis",
+                maxWidth: 200, // Adjust the maxWidth as needed
+                display: 'inline-block',
+              }}
+            >
+              {item.name}
+            </Text>
+          </Tooltip>
         </Stack>
       </BodyCell>
       <BodyCell align="left" sx={{ paddingLeft: 0 }}>
