54b3a8eba191d79a8e8fb1c430f6f7c7c5df4e0f
SangLM <Sanglm2207@gmail.com>
2024-12-30 01:53:43 +0700

    feat: enhance AddBudget component with selectedBudget prop and implement placeholder functions for budget handling

--- src/components/sn-project-detail/Budget/Actions/AddBudget.tsx
+++ src/components/sn-project-detail/Budget/Actions/AddBudget.tsx
@@ -1,13 +1,14 @@
 "use client";
 
-import { memo } from "react";
-import PlusIcon from "../../../../icons/PlusIcon";
 import { Button } from "components/shared";
-import { useTranslations } from "next-intl";
+import ModalAddBudget from "components/sn-project-detail/Budget/Actions/ModalAddBudget";
 import { NS_PROJECT } from "constant/index";
 import useToggle from "hooks/useToggle";
-import ModalAddBudget from "components/sn-project-detail/Budget/Actions/ModalAddBudget";
 import AddSquareIcon from "icons/AddSquareIcon";
+import { useTranslations } from "next-intl";
+import { memo } from "react";
+import { TBudgetListQueries } from "store/project/budget/action";
+import PlusIcon from "../../../../icons/PlusIcon";
 
 const AddBudget = ({ projectId }: { projectId?: string }) => {
   const projectT = useTranslations(NS_PROJECT);
@@ -67,7 +68,21 @@ const AddBudget = ({ projectId }: { projectId?: string }) => {
         open={isOpenModalAddBudget}
         onClose={hideModalAddBudget}
         projectId={projectId}
-      />
+        onAddSnackbar={function (message: string, severity?: "error" | "success" | "info" | "warning", expiredIn?: number): void {
+          throw new Error("Function not implemented.");
+        }}
+        onGetBudget={function (queries?: TBudgetListQueries): Promise<void> {
+          throw new Error("Function not implemented.");
+        }}
+        selectedBudget={{
+          id: undefined,
+          project_id: "",
+          name: "",
+          start_date: "",
+          end_date: "",
+          owner: "",
+          client: ""
+        }} />
     </>
   );
 };
