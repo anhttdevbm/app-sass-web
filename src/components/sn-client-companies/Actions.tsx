f25169ffd89e6030d13fb2a76df8fc00bed369e2
SangLM <Sanglm2207@gmail.com>
2024-12-31 15:39:49 +0700

    refactor: update file upload method in Actions component for improved functionality

--- src/components/sn-client-companies/Actions.tsx
+++ src/components/sn-client-companies/Actions.tsx
@@ -77,7 +77,7 @@ const Actions = () => {
   const onUpdate = async (data: ClientCompany) => {
     const payload = { ...data };
     if (data.files) {
-      const logoUrl = await client.upload(Endpoint.UPLOAD, data?.files);
+      const logoUrl = await client.uploadFileV2(Endpoint.UPLOAD_FILE_V2, data?.files);
       payload.avatar = [logoUrl];
     } else {
       delete payload["files"];
