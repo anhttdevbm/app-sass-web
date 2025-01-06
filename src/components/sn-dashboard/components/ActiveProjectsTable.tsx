b6d6b07eb6f8eae2422db6c1a0e155ca72b2aca8
SangLM <Sanglm2207@gmail.com>
2025-01-06 15:29:49 +0700

    refactor: simplify key generation in ActiveProjectsTable component

--- src/components/sn-dashboard/components/ActiveProjectsTable.tsx
+++ src/components/sn-dashboard/components/ActiveProjectsTable.tsx
@@ -43,8 +43,8 @@ function ActiveProjectsTable() {
                     </TableRow>
                 </TableHead>
                 <TableBody>
-                    {data && data.map((row, index) => (
-                        <TableRow key={`${row.id}-${index}`}>
+                    {data && data.map((row) => (
+                        <TableRow key={row.id}>
                             <BodyCell>
                                 <Typography textAlign='left' fontWeight={600} fontSize={14}>
                                     {row.name}
